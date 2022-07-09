efed62343e5f406b5021657dfd948906708d51b7
Benjamin Mordaunt <crawford.benjamin15@gmail.com>
2022-07-09 23:20:24 +0100

    info: add long long size
    Show length of the long long type in the info program.
    
    Signed-off-by: Benjamin Mordaunt <crawford.benjamin15@gmail.com>

--- xv6-armv8/usr/info.c
+++ xv6-armv8/usr/info.c
@@ -8,9 +8,10 @@
 int
 main(void)
 {
-    printf(1, "Size of char:  %d byte%s\n", sizeof(char), sizeof(char) > 1 ? "s" : "");
-    printf(1, "Size of short: %d byte%s\n", sizeof(short), sizeof(short) > 1 ? "s" : "");
-    printf(1, "Size of int:   %d byte%s\n", sizeof(int), sizeof(int) > 1 ? "s" : "");
-    printf(1, "Size of long:  %d byte%s\n", sizeof(long), sizeof(long) > 1 ? "s" : "");
+    printf(1, "Size of char:      %d byte%s\n", sizeof(char), sizeof(char) > 1 ? "s" : "");
+    printf(1, "Size of short:     %d byte%s\n", sizeof(short), sizeof(short) > 1 ? "s" : "");
+    printf(1, "Size of int:       %d byte%s\n", sizeof(int), sizeof(int) > 1 ? "s" : "");
+    printf(1, "Size of long:      %d byte%s\n", sizeof(long), sizeof(long) > 1 ? "s" : "");
+    printf(1, "Size of long long: %d byte%s\n", sizeof(long long), sizeof(long long) > 1 ? "s" : "");
     exit();
 }
