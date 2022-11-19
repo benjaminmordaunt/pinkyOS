72705c28891834960ca7b75b9f83cd521c35a71d
Benjamin Mordaunt <crawford.benjamin15@gmail.com>
2022-11-19 04:15:50 +0000

    feature(buddy): expand on kmem buddy allocator

--- src/kernel/include/kmem.h
+++ src/kernel/include/kmem.h
@@ -1,4 +1,13 @@
 #define MIN(a, b) ((a < b) ? (a) : (b))
 #define MAX(a, b) ((a > b) ? (a) : (b))
 
+typedef struct list_head_struct {
+    struct list_head_struct *next;
+    struct list_head_struct *prev;
+} list_head_t;
+
+#define list_head_init(l) \
+            ((list_head_t *)l)->next = 0; \
+            ((list_head_t *)l)->prev = 0;
+
 int kstrcmp(const char *p, const char *q);
--- src/kernel/kmem.c
+++ src/kernel/kmem.c
@@ -13,7 +13,8 @@
 // limitations under the License.
 //
 
-/* A simple implementation of a buddy allocator to kickstart development of pinkyOS.
+/* A simple implementation of a uni-processor (no notion of per-CPU pagesets) 
+   buddy allocator to kickstart development of pinkyOS.
  */
 
 #include "kmem.h"
@@ -26,7 +27,7 @@
         of up to a single 4GiB contiguous space.
 */
 
-#define PM_BUDDY_MAX_ORDER                             20       
+#define PM_BUDDY_MAX_ORDER                             10       
 #define PM_BUDDY_BM_OFFSET(order, maxord)               \
             ((uint32_t)(1 << (MAX(maxord - order, 0))))
 
@@ -34,7 +35,7 @@
 struct pm_extent {
     va_t start;
     va_t end;
-    struct vm_extent *next;
+    list_head_t lentry;
 };
 
 /* Represents a collection of pm_extent. Typically used
@@ -42,68 +43,67 @@ struct pm_extent {
    by the bootloader. */
 /* XXX: Standard linked list API for pinkyOS. */
 
-struct pm_buddy_block {
-    struct pm_buddy_block *next; /* References next free block, possibly its buddy. */
-    struct pm_buddy_block *prev; /* References previous free block, possibly its buddy.*/
-};
-
 /* A single order manages memory of granularity 2^ord, where ord
    is the index of the order in the parent pm_physmap.orders array. 
    alloc_head references a null-pointer, or a pointer to the first 
    pm_buddy_block structure if at least one (two with its buddy) is
    present in the buddy system. */
-struct pm_buddy_order {
-    uint32_t              *bitmap; /* The "base address" of this order in the global bitmap */
-};
 
 struct pm_physmap {
-    uint32_t              *journal_start;
-    uint32_t              *heap_start;
-    struct pm_buddy_order orders[PM_BUDDY_MAX_ORDER + 1];
+    void                 *heap_start;
+    list_head_t           orders[PM_BUDDY_MAX_ORDER];
     int                   maxord;
 } pm_physmap_up;
+
+int _pm_physmap_init(void *start, pa_t size, int order, struct pm_extent *keepout_head) {
+    struct pm_extent *keepout = keepout_head;
+    pa_t ordsz = (1 << (_PT_PS + order));
+    
+    list_head_init(start);
+
+    /* pm_physmap_init already guaranteed size will be a PAGE_SIZE multiple */
+    pm_physmap_free_region(start, start + size, start + ordsz);
+
+    for (; keepout; keepout = keepout->lentry.next) {
+        if (keepout->start >= keepout->end)
+            panic("pm_physmap_init: malformed or zero-size keepout entry");
+
+        /* Check whether hole is completely irrelevant */
+        if ((keepout->start < start && keepout->end <= start)
+         || (keepout->start >= start + ordsz))
+            continue;
+
+        pm_physmap_free_region(start, keepout->start, keepout->end);
+    }
+
+    return 0;
+}
 
 int pm_physmap_init(struct pm_extent *physmem_ext, struct pm_extent *keepout_head) {
     pa_t physmem_sz = physmem_ext->end - physmem_ext->start;
     struct pm_extent *keepout = keepout_head;
     struct pm_physmap *pmap = &pm_physmap_up;
-    struct pm_buddy_order ord;
-    int nbitmaps, maxord, i;
+    struct list_head_t *ord;
+    int maxord, i, rc;
 
     if (physmem_ext->start != ALIGN_DOWN(physmem_ext->start, _PT_PS)
     || (physmem_ext->end   != ALIGN_DOWN(physmem_ext->end,   _PT_PS))) {
         panic("pm_physmap_init: addressable extent vas must be at least page aligned");
     }
 
-    if (physmem_sz & ((1 << PM_BUDDY_MAX_ORDER) - 1) != 0)
+    if (physmem_sz & ((1 << (PM_BUDDY_MAX_ORDER + _PT_PS)) - 1) != 0)
         panic("pm_physmap_init: addressable range cannot be covered by buddy allocator");
 
-    pmap->journal_start = (uint32_t *)physmem_ext->start;
-    
-    /* Need to know nbitmaps to find how large the bookkeeping journal needs to be,
-       but the more we eat into the heap, the (potentially) fewer bitmaps we need.
-       Just overprovision and avoid the nasty math for now. */
-    nbitmaps = 1 << MAX(ffs(physmem_sz - 1) - _PT_PS - 3, 0);
-    if (physmem_sz < nbitmaps * sizeof(uint32_t))
-        panic("pm_physmap_init: insufficient address space for buddy bookkeeping");
+    pmap->heap_start = (uint32_t *)physmem_ext->start;
 
-    for (i = 0; i < nbitmaps; i++) {
-        pmap->journal_start[i] = UINT32_MAX;
-    }
+    maxord = ffs(physmem_sz) + 1 - _PT_PS;
+    rc = _pm_physmap_init(pmap->heap_start, maxord, keepout_head);
 
-    pmap->heap_start = &pmap->journal_start[i]; 
-    
-    /* Still cannot update physmem_sz as there cannot be disparity between maxord
-       and the size of the bitmap for the bitmap offset calc routines. */
-    pmap->maxord = MAX(ffs(physmem_sz - 1) + 1 - _PT_PS, 0);
-    for (i = 0; i < maxord; i++) {
-        pmap->orders[i].bitmap = pmap->journal_start + PM_BUDDY_BM_OFFSET(i, maxord);
-    }
-    pmap->orders[i] = (struct pm_buddy_order){ 0 };
-
-    for (; keepout; keepout = keepout->next) {
+    for (; keepout; keepout = keepout->lentry.next) {
         pm_physmap_alloc(keepout->start, keepout->end - keepout->start, PM_KEEPOUT);
     }
+
+    return rc;
 }
 
 // ----- TO BE MOVED BEGIN -----
